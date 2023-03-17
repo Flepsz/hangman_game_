9551fb69ca7adf80494cbdff489e5eb845435463
Felipe Pereira <70305395+Flepsz@users.noreply.github.com>
2023-03-17 16:38:23 -0300

    Main exec

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from gamemodes import *
+
+
+def main():
+    print("Welcome to Hangman Game!")
+    while True:
+        level = get_level()
+        if level == "Nutella":
+            play_nutella()
+        elif level == "Café com leite":
+            play_cafe_com_leite()
+        elif level == "Raiz":
+            play_raiz()
+        play_again = input("{}Do you want to play again?{} ({}Y{}/{}N{}) ".format(color['cyanz'], color['r'],
+                                                                                  color['green'], color['r'],
+                                                                                  color['red'], color['r'])).lower()
+        print("\n")
+        print("->"*20)
+        if play_again != "y":
+            print("Thanks for playing!")
+            break
+
+
+if __name__ == '__main__':
+    main()
